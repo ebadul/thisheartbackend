33cff93b21c5db8a95026e8ae4d218adf4015439
mamun-git-14 <al.mamun74bd@gmail.com>
2019-02-25 14:23:11 +0600

    Base Source for commit

---
+++ app/Http/Controllers/API/AccountController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\Account;
+use Validator;
+
+
+class AccountController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $accounts = Account::all();
+
+        return $this->sendResponse($accounts->toArray(), 'Accounts retrieved successfully.');
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'name' => 'required',
+            'detail' => 'required'
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+
+        $account = Account::create($input);
+
+        return $this->sendResponse($account->toArray(), 'Account created successfully.');
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $account = Account::find($id);
+
+
+        if (is_null($account)) {
+            return $this->sendError('Account not found.');
+        }
+
+
+        return $this->sendResponse($account->toArray(), 'Account retrieved successfully.');
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Account $account)
+    {
+        $input = $request->all();
+
+
+        $validator = Validator::make($input, [
+            'name' => 'required',
+            'detail' => 'required'
+        ]);
+
+
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+
+
+        $account->name = $input['name'];
+        $account->detail = $input['detail'];
+        $account->save();
+
+
+        return $this->sendResponse($account->toArray(), 'Account updated successfully.');
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Account $account)
+    {
+        $account->delete();
+
+
+        return $this->sendResponse($account->toArray(), 'Account deleted successfully.');
+    }
+}
---
+++ app/Http/Controllers/API/AuthenticationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Lcobucci\JWT\Parser;
+use Carbon\Carbon;
+use Validator;
+use Illuminate\Support\Facades\Log;
+
+
+class AuthenticationController extends BaseController
+{
+    public function login(Request $request){
+        $user = User::where('email',$request->email)->first();
+        Log::info("Email = ".$request->email);
+        Log::info("Password = ".$request->password);
+        Log::info("User Name = ".$user->name);
+
+        if($user){
+            
+            $passwordOK = Hash::check($request->password, $user->password);
+            if($passwordOK){
+                $tokenResult = $user->createToken('ThisHeartAccessToken');
+                
+                return response()->json([
+                    'message' => 'User logged in successfully!',
+                    'access_token' => $tokenResult->accessToken,
+                    'token_type' => 'Bearer',
+                    'user_name' => $user->name,
+                    'expires_at' => Carbon::parse($tokenResult->token->expires_at)->toDateTimeString()
+                ], 200);
+            }else{
+                return response()->json([
+                    'message' => 'Password mismatch!'
+                ], 422);
+            }
+        }else{
+            response()->json([
+                'message' => 'Email not exist!',
+            ], 401);
+        }
+    }
+     
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+        return response()->json([
+            'message' => 'Successfully logged out'
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $tokenResult = $user->createToken('ThisHeartAccessToken');
+        $success['name'] =  $user->name;
+
+        return response()->json([
+            'message' => 'User registered successfully!',
+            'access_token' => $tokenResult->accessToken,
+            'token_type' => 'Bearer',
+            'user_name' => $user->name,
+            'expires_at' => Carbon::parse($tokenResult->token->expires_at)->toDateTimeString()
+        ], 200);
+    }
+
+    
+}
