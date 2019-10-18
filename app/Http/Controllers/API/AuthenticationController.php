52997e78d2d0fa93892cb4b1d279d1b05fbb2e68
mamun-git-14 <al.mamun74bd@gmail.com>
2019-10-18 19:57:42 +0600

    Encode and Decode set for Main user, Accout and Beneficiary user.

--- app/Http/Controllers/API/AuthenticationController.php
+++ app/Http/Controllers/API/AuthenticationController.php
@@ -14,6 +14,7 @@ use Carbon\Carbon;
 use Validator;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Crypt;
 
 
 class AuthenticationController extends BaseController
@@ -21,7 +22,7 @@ class AuthenticationController extends BaseController
     public function login(Request $request){
         
         $user = User::where('email', '=', $request->email)->first();
-        //Log::info("Email = ".$request->email);
+        Log::info("Email = ".$request->email);
         //Log::info("Password = ".$request->password);
 
         if($user === null){
@@ -42,7 +43,7 @@ class AuthenticationController extends BaseController
                     return response()->json([
                         'message' => 'User logged in successfully!',
                         'user_id' => $user->id,
-                        'user_name' => $user->name,
+                        'user_name' => Crypt::decryptString($user->name),
                         'access_token' => $tokenResult->accessToken,
                         'account_progress_status' => $accountProgressStatus,
                         'token_type' => 'Bearer',
@@ -60,7 +61,148 @@ class AuthenticationController extends BaseController
             }
         }
     }
+     
+    public function logout()
+    {
+        Auth::user()->token()->revoke();
+        
+        return response()->json([
+            'message' => 'Successfully logged out'
+        ]);
+    }
 
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+
+        $userData = User::where('email', '=', $request->email)->first();
+        if($userData){
+            return response()->json([
+                'message' => 'Email already exist. Please use another.',
+            ], 406);
+        }
+
+        $input = $request->all();
+        $input['name'] = Crypt::encryptString($request->name);
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $tokenResult = $user->createToken('ThisHeartAccessToken');
+
+        return response()->json([
+            'message' => 'User registered successfully!',
+            'access_token' => $tokenResult->accessToken,
+            'token_type' => 'Bearer',
+            'user_name' => Crypt::decryptString($user->name),
+            'user_id' => $user->id,
+            'expires_at' => Carbon::parse($tokenResult->token->expires_at)->toDateTimeString()
+        ], 200);
+    }
+
+    public function registerBeneficiaryUser(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+            'user_id' => 'required',
+            'beneficiary_id' => 'required'
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+
+        $beneficiaryInfo = Beneficiary::where('id', '=', $request->beneficiary_id)->first();
+        if($beneficiaryInfo){
+
+            if($beneficiaryInfo->validate_code == 0){
+                return response()->json([
+                    'message' => 'Your access code not validate yet. Please validate code then register.'
+                ],400);
+            }
+
+            if(Crypt::decryptString($beneficiaryInfo->last_4_beneficiary) == $request->last4social_code){
+            }else{
+                return response()->json([
+                    'message' => 'Invalid social code. Please try again.'
+                ],401);
+            }
+           
+        }
+
+        $userData = BeneficiaryUser::where('email', '=', $request->email)->first();
+        if($userData){
+            return response()->json([
+                'message' => 'Email already exist. Please use another.',
+            ], 406);
+        }
+
+        $userData = BeneficiaryUser::where('beneficiary_id', '=', $request->beneficiary_id)->first();
+        if($userData){
+            return response()->json([
+                'message' => 'You have already account ['.$userData->email.'] for this last 4 social code.',
+            ], 402);
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+
+        $beneficiaryUser = new BeneficiaryUser();
+        $beneficiaryUser->email = $request->email;
+        $beneficiaryUser->user_id = $request->user_id;
+        $beneficiaryUser->beneficiary_id = $request->beneficiary_id;
+        $beneficiaryUser->password = $input['password'];
+        $beneficiaryUser->save();
+
+        return response()->json([
+            'message' => 'User registered successfully!',
+            'data' => $beneficiaryUser
+        ], 200);
+    }
+
+    public function loginBeneficiaryUser(Request $request){
+
+        $user = BeneficiaryUser::where('email', '=', $request->email)->first();
+        //Log::info("Email = ".$request->email);
+        //Log::info("Password = ".$request->password);
+
+        if($user === null){
+            return response()->json([
+                'message' => 'Email not exist!',
+            ], 401);
+        }else{
+            $passwordOK = Hash::check($request->password, $user->password);
+            if($passwordOK){
+                $tokenResult = $user->createToken('ThisHeartAccessToken');
+
+                $beneficiaryInfo = Beneficiary::findOrfail($user->beneficiary_id);
+                
+                return response()->json([
+                    'message' => 'User logged in successfully!',
+                    'user_id' => $user->id,
+                    'primary_user_id' => $user->user_id,
+                    'user_name' => Crypt::decryptString($beneficiaryInfo->first_name) ." ".Crypt::decryptString($beneficiaryInfo->last_name),
+                    'beneficiary_id' => $user->beneficiary_id,
+                    'access_token' => $tokenResult->accessToken,
+                    'token_type' => 'Bearer',
+                    'expires_at' => Carbon::parse($tokenResult->token->expires_at)->toDateTimeString()
+                ], 200);
+            }else{
+                return response()->json([
+                    'message' => 'Password mismatch!'
+                ], 422);
+            }
+        }
+    }
+
+    
     function checkAccountProgressData($user_id){
 
         $allDataCompleted = true;
@@ -107,145 +249,4 @@ class AuthenticationController extends BaseController
 
         return $allDataCompleted;
     }
-     
-    public function logout()
-    {
-        Auth::user()->token()->revoke();
-        
-        return response()->json([
-            'message' => 'Successfully logged out'
-        ]);
-    }
-
-    public function register(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required',
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
-
-        if($validator->fails()){
-            return $this->sendError('Validation Error.', $validator->errors());       
-        }
-
-        $userData = User::where('email', '=', $request->email)->first();
-        if($userData){
-            return response()->json([
-                'message' => 'Email already exist. Please use another.',
-            ], 406);
-        }
-
-        $input = $request->all();
-        $input['password'] = bcrypt($input['password']);
-        $user = User::create($input);
-        $tokenResult = $user->createToken('ThisHeartAccessToken');
-        $success['name'] =  $user->name;
-
-        return response()->json([
-            'message' => 'User registered successfully!',
-            'access_token' => $tokenResult->accessToken,
-            'token_type' => 'Bearer',
-            'user_name' => $user->name,
-            'user_id' => $user->id,
-            'expires_at' => Carbon::parse($tokenResult->token->expires_at)->toDateTimeString()
-        ], 200);
-    }
-
-    public function registerBeneficiaryUser(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|email',
-            'password' => 'required',
-            'user_id' => 'required',
-            'beneficiary_id' => 'required'
-        ]);
-
-        if($validator->fails()){
-            return $this->sendError('Validation Error.', $validator->errors());       
-        }
-
-        $beneficiaryInfo = Beneficiary::where('id', '=', $request->beneficiary_id)->first();
-        if($beneficiaryInfo){
-
-            if($beneficiaryInfo->validate_code == 0){
-                return response()->json([
-                    'message' => 'Your access code not validate yet. Please validate code then register.'
-                ],400);
-            }
-
-            if($beneficiaryInfo->last_4_beneficiary == $request->last4social_code){
-            }else{
-                return response()->json([
-                    'message' => 'Invalid social code. Please try again.'
-                ],401);
-            }
-           
-        }
-
-        $userData = BeneficiaryUser::where('email', '=', $request->email)->first();
-        if($userData){
-            return response()->json([
-                'message' => 'Email already exist. Please use another.',
-            ], 406);
-        }
-
-        $userData = BeneficiaryUser::where('beneficiary_id', '=', $request->beneficiary_id)->first();
-        if($userData){
-            return response()->json([
-                'message' => 'You have already account ['.$userData->email.'] for this last 4 social code.',
-            ], 402);
-        }
-
-        $input = $request->all();
-        $input['password'] = bcrypt($input['password']);
-
-        $beneficiaryUser = new BeneficiaryUser();
-        $beneficiaryUser->email = $request->email;
-        $beneficiaryUser->user_id = $request->user_id;
-        $beneficiaryUser->beneficiary_id = $request->beneficiary_id;
-        $beneficiaryUser->password = $input['password'];
-        $beneficiaryUser->save();
-
-        return response()->json([
-            'message' => 'User registered successfully!',
-            'data' => $beneficiaryUser
-        ], 200);
-    }
-
-    public function loginBeneficiaryUser(Request $request){
-        $user = BeneficiaryUser::where('email', '=', $request->email)->first();
-        //Log::info("Email = ".$request->email);
-        //Log::info("Password = ".$request->password);
-
-        if($user === null){
-            return response()->json([
-                'message' => 'Email not exist!',
-            ], 401);
-        }else{
-            $passwordOK = Hash::check($request->password, $user->password);
-            if($passwordOK){
-                $tokenResult = $user->createToken('ThisHeartAccessToken');
-
-                $beneficiaryInfo = Beneficiary::findOrfail($user->beneficiary_id);
-                
-                return response()->json([
-                    'message' => 'User logged in successfully!',
-                    'user_id' => $user->id,
-                    'primary_user_id' => $user->user_id,
-                    'user_name' => $beneficiaryInfo->first_name ." ".$beneficiaryInfo->last_name,
-                    'beneficiary_id' => $user->beneficiary_id,
-                    'access_token' => $tokenResult->accessToken,
-                    'token_type' => 'Bearer',
-                    'expires_at' => Carbon::parse($tokenResult->token->expires_at)->toDateTimeString()
-                ], 200);
-            }else{
-                return response()->json([
-                    'message' => 'Password mismatch!'
-                ], 422);
-            }
-        }
-    }
-
-    
 }
