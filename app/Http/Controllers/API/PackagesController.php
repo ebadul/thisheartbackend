0852f32c0014abff9fb7283cb1b704a899931a1f
Shahin Islam Fakhrul <shahin2k5@gmail.com>
2020-02-10 21:08:42 +0600

    Package details

---
+++ app/Http/Controllers/API/PackagesController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Auth;
+use App\Services\OTPService;
+use App\WizardStep;
+use App\PackageInfo;
+use App\PackageEntity;
+use App\UserPackage;
+use Illuminate\Support\Facades\Validator;
+ 
+
+class PackagesController extends Controller
+{
+    
+    public function getPackages(){
+        $packages = PackageInfo::all();
+        return response()->json([
+            'status'=>'success',
+            'data'=>$packages
+        ]);
+    }
+
+    public function savePackageInfo(Request $rs){
+        $sub_plan = $rs->sub_plan;
+        $user = Auth::user();
+        if(!empty($sub_plan)){
+            $user_id = $user->id;
+            $pkgData = [
+                'user_id'=>$user_id,
+                'package_id'=>$sub_plan
+            ];
+            $user_package = new UserPackage;
+            $user_pkg = $user_package->saveUserPackage($pkgData);
+            $user_pkg->push('package_info',$user_pkg->package_info);
+            return response()->json([
+                'status'=>'success',
+                'package_info'=>$user_pkg->package_info,
+                'sub_plan'=>$user_pkg,
+            ], 200);
+        }
+    }
+
+    public function savePaymentInfo(Request $rs){
+        
+            return response()->json([
+                'status'=>'success',
+                'package_info'=>$rs->all(),
+            ], 200);
+      
+    }
+
+    public function package_info(){
+        $package_info = PackageInfo::all();
+        $user = Auth::user();
+        return view('admin.package_info',['user'=>$user,'package_info'=>$package_info]);
+    }
+
+    public function package_entities($package_id){
+        $package_entity = PackageEntity::where('package_id','=',$package_id)->get();
+        $user = Auth::user();
+        return view('admin.package_entity',['user'=>$user,'package_entity'=>$package_entity]);
+    }
+
+    public function user_package(){
+        $user = Auth::user();
+        $user_package = UserPackage::all();
+        return view('admin.user_package',['user'=>$user,'user_package'=>$user_package]);
+    }
+
+    // public function setSteps(Request $rs){
+    //     $user = Auth::user();
+    //     $step = $rs->step;
+    //     $info = $rs->info;
+    //     $wizStep = WizardStep::where('user_id','=',$user->id)->where('steps',$rs->step)->first();
+    //     if(empty($wizStep)){
+    //         $wizStep = new WizardStep;
+    //     }
+    //     $wizStep->user_id = $user->id;
+    //     $wizStep->steps = $rs->step;
+    //     $wizStep->status = 1;
+    //     $wizStep->info = $rs->info;
+    //     $wizStep->save();
+
+    //     return response()->json([
+    //         'status'=>'success',
+    //         'data'=>$wizStep
+    //     ]);
+    // }
+}
---
+++ resources/views/admin/left-sidebar.blade.php
@@ -0,0 +1,54 @@
+
+
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="{{asset('AdminLTE/dist/img/userphoto.jpg')}}" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p>{{$user->email}}</p>
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+      <!-- search form -->
+      <form action="#" method="get" class="sidebar-form">
+        <div class="input-group">
+          <input type="text" name="q" class="form-control" placeholder="Search...">
+          <span class="input-group-btn">
+                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                </button>
+              </span>
+        </div>
+      </form>
+      <!-- /.search form -->
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="header">MAIN NAVIGATION</li>
+        <li class="active treeview">
+          <a href="#">
+            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+          <!-- <li class="active"><a href="dashboard"><i class="fa fa-circle-o"></i> Dashboard</a></li> -->
+            <li ><a href="primary_user"><i class="fa fa-circle-o"></i> Primary User's</a></li>
+            <li><a href="beneficiary_user"><i class="fa fa-circle-o"></i> Benefiicary User's</a></li>
+            <li><a href="/package_info"><i class="fa fa-circle-o"></i> Package Info</a></li>
+            <li><a href="/user_package"><i class="fa fa-circle-o"></i> User Package</a></li>
+          </ul>
+        </li>
+
+        <li class="header">Settings</li>
+        <li><a href="{{route('logout')}}"><i class="fa fa-circle-o text-red"></i> <span>Log Out</span></a></li>
+        <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Forgot Password</span></a></li>
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
+
+
