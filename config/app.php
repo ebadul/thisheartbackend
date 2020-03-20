2f4885b93c553e488d78d715ae30ee9ae458a053
Shahin Islam Fakhrul <shahin2k5@gmail.com>
2020-03-20 12:38:12 +0600

    Toast Provider calss missing fix

--- config/app.php
+++ config/app.php
@@ -176,7 +176,6 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\TwilioServiceProvider::class,
         PragmaRX\Google2FALaravel\ServiceProvider::class,
-        Grimthorr\LaravelToast\ServiceProvider::class,
     ],
 
     /*
